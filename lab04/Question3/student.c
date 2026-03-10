6e7526ef7d9c24e4a2f2db80558c7018fd757874
Steven B. <s.brashear1234@gmail.com>
2026-03-10 08:55:32 -0500

    Completed 03/10/2026

--- lab04/Question3/student.c
+++ lab04/Question3/student.c
@@ -55,7 +55,21 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
+int dfs(struct TreeNode* node, int current) {
+    if (node == NULL) {
+        return 0;
+    }
+
+    current = current * 10 + node->val;
+
+    // if leaf node
+    if (node->left == NULL && node->right == NULL) {
+        return current;
+    }
+
+    return dfs(node->left, current) + dfs(node->right, current);
+}
 
 int sumNumbers(struct TreeNode* root) {
-      // TODO: implement
+    return dfs(root, 0);
 }
