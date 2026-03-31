9a8435cc53bec80737a3d1311b3568530ab9406b
Steven B. <s.brashear1234@gmail.com>
2026-03-31 08:32:16 -0500

    Finished On 03/31/2026

--- lab06/Question2/student.c
+++ lab06/Question2/student.c
@@ -13,7 +13,24 @@
 int count_isolated(Graph* g) {
     // TODO: implement
     // return -1;
-   
+    int count = 0;
+
+    for (int i = 0; i < MAX_NODES; i++) {
+        int is_isolated = 1;  // assume isolated
+
+        for (int j = 0; j < MAX_NODES; j++) {
+            if (g->adj[i][j] != 0) {
+                is_isolated = 0;  // found an edge
+                break;
+            }
+        }
+
+        if (is_isolated) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 
